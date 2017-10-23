a54abc63248fdf41a920c1d493df294c40f2eb29
Chu Rong <roychu.r@gmail.com>
2017-10-23 15:40:07 -0700

    refactoring digits/tools/parse_s3.py for unit test

--- digits/tools/parse_s3.py
+++ digits/tools/parse_s3.py
@@ -25,9 +25,8 @@ def unescape(s):
     return urllib.unquote(s)
 
 
-def validate_s3(endpoint, bucket, path, accesskey, secretkey):
+def validate_s3(walker, bucket, path):
     try:
-        walker = S3Walker(endpoint, accesskey, secretkey)
         walker.connect()
         if not path.endswith('/'):
             path = path + '/'
@@ -279,7 +278,7 @@ def three_way_split_indices(size, pct_b, pct_c):
         return a, a + b
 
 
-def parse_s3(endpoint, bucket, path, accesskey, secretkey, labels_file,
+def parse_s3(walker, bucket, path, labels_file,
                  train_file=None, percent_train=None,
                  val_file=None, percent_val=None,
                  test_file=None, percent_test=None,
@@ -321,7 +320,6 @@ def parse_s3(endpoint, bucket, path, accesskey, secretkey, labels_file,
                     labels.append(line)
 
     # Verify that at least two category folders exist
-    walker = S3Walker(endpoint, accesskey, secretkey)
     walker.connect()
     if not path.endswith('/'):
         path = path + '/'
@@ -521,9 +519,9 @@ if __name__ == '__main__':
     )
 
     args = vars(parser.parse_args())
-
+    walker = S3Walker(args['endpoint'], args['accesskey'], args['secretkey'])
     for valid in [
-            validate_s3(args['endpoint'], args['bucket'], args['path'], args['accesskey'], args['secretkey']),
+            validate_s3(walker, args['bucket'], args['path']),
             validate_range(args['percent_train'],
                            min_value=0, max_value=100, allow_none=True),
             validate_output_file(args['train_file']),
@@ -547,7 +545,8 @@ if __name__ == '__main__':
 
     start_time = time.time()
 
-    if parse_s3(args['endpoint'], args['bucket'], args['path'], args['accesskey'], args['secretkey'], args['labels_file'],
+    
+    if parse_s3(walker, args['bucket'], args['path'], args['labels_file'],
                     train_file=args['train_file'],
                     percent_train=percent_train,
                     val_file=args['val_file'],
